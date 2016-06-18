2f01a50006fcd0da5af0d129365f9f1b9df4d703
Kuaaaly <quentin@legraverend.fr>
2016-06-18 15:22:42 +0200

    Revert "Ignore everything except .py files"
    This reverts commit abeaf043c22eae77026478e28a01140b37f3879d.

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 # Kuaaaly - 18 Juin 2016
-# Python 2.7.10
 
 import os
 import glob
-import zipfile
 
 ipa_files = glob.glob('*.ipa')
 
 for name in ipa_files:
 	root, ext = os.path.splitext(name)
 	os.rename(name, root + '.zip')
-
-zip_files = glob.glob('*.zip')
-
-for name in zip_files:
-	zip_ref = zipfile.ZipFile(name, 'r')
-	zip_ref.extractall('./')
-	zip_ref.close()
-
-	#csv_list = os.listdir('./Payload/Clash of Clans.app/res/logic')
-
-wanted_csv = ['buildings.csv', 'heroes.csv', 'spells.csv', 'traps.csv', 'troops.csv']
